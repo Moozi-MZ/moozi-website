1205d905f433a9db3f048733c2e54bc007fa4f16
unknown <leonardocolacogg@gmail.com>
2025-03-28 15:08:49 +0200

    early stage demo

--- src/components/Features/FeatureTwo.jsx
+++ src/components/Features/FeatureTwo.jsx
@@ -1,4 +1,6 @@
 import MOCKUP_BKG from '../../assets/mockups/bkg.png'
+import CUSTOMER_SUPPORT from '../../assets/illustrations/call-center-animate.svg';
+import './FeatureTwo.css'
 
 export const FeatureTwo = () => {
     return (
@@ -36,7 +38,7 @@ export const FeatureTwo = () => {
               <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-t-[2rem]"></div>
               <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
                 <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Performance</p>
+                  <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Fast Response Time</p>
                   <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit maiores impedit.
                   </p>
@@ -75,24 +77,20 @@ export const FeatureTwo = () => {
               <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
                 <div className="px-8 pt-8 pb-3 sm:px-10 sm:pt-10 sm:pb-0">
                   <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">
-                    Powerful APIs
+                    Great Customer Support
                   </p>
                   <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
                     Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem sodales gravida.
                   </p>
                 </div>
+
                 <div className="relative min-h-[30rem] w-full grow">
-                  <div className="absolute top-10 right-0 bottom-0 left-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
-                    <div className="flex bg-gray-800/40 ring-1 ring-white/5">
-                      <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
-                        <div className="border-r border-b border-r-white/10 border-b-white/20 bg-white/5 px-4 py-2 text-white">
-                          NotificationSetting.jsx
-                        </div>
-                        <div className="border-r border-gray-600/10 px-4 py-2">App.jsx</div>
+                  
+                      <div className="customer-support-graphic">
+                          <img src={CUSTOMER_SUPPORT} width={350} />
                       </div>
-                    </div>
-                    <div className="px-6 pt-6 pb-14">{/* Your code example */}</div>
-                  </div>
+                    
+                  
                 </div>
               </div>
               <div className="pointer-events-none absolute inset-px rounded-lg ring-1 shadow-sm ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
